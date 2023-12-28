547c80d1e0ce6e4c679ba82997198e034a196250
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-12-29 00:18:39 +0100

    removed server session temporarily
    need to figure out how to do this securely

--- routes/identity_routes.py
+++ routes/identity_routes.py
@@ -13,25 +13,6 @@ def get_identity():
         return "could not construct user data"
     return flask.jsonify(user_data)
 
-
-@identity_blueprint.post("/me/server")
-def get_server_identity():
-    args = dict(flask.request.json)
-    session_hash = args.get("session_hash")
-    user_id = args.get("user_id")
-    if None in [session_hash, user_id]:
-        return "missing arguments", 400    
-    
-    user_data = utils.session.get_server_session(session_hash)
-    if not user_data:
-        return "could not construct user data"
-    
-    if user_data.get("user_id") != user_id:
-        return "invalid arguments", 401
-    
-    return flask.jsonify(user_data)
-
-
 @identity_blueprint.get("/user/<int:id>")
 def get_foreign_user(id: int):
     if not flask.session.get("server_session_value"):
@@ -39,3 +20,20 @@ def get_foreign_user(id: int):
     foreign_user_data = utils.session.get_foreign_user_data(id)
     return flask.jsonify(foreign_user_data)
     
+
+# @identity_blueprint.post("/me/server")
+# def get_server_identity():
+#     args = dict(flask.request.json)
+#     session_hash = args.get("session_hash")
+#     user_id = args.get("user_id")
+#     if None in [session_hash, user_id]:
+#         return "missing arguments", 400    
+    
+#     user_data = utils.session.get_server_session(session_hash)
+#     if not user_data:
+#         return "could not construct user data"
+    
+#     if user_data.get("user_id") != user_id:
+#         return "invalid arguments", 401
+    
+#     return flask.jsonify(user_data)
