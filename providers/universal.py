22f43c57d3a00c2232c82d3fff01f3621468b86f
human <70441087+human-sysdev@users.noreply.github.com>
2024-01-06 13:31:49 +0100

    Cleanup

--- providers/universal.py
+++ providers/universal.py
@@ -11,14 +11,15 @@ def append_signin_code(request: flask.Request) -> utils.database.Database.LoginR
     state = args.get("state")
     if None in [code, state]:
         return None
-    
+
     login_request = utils.database.Database().get_login_request_by_state(state)
     if not login_request:
         return None
-    
+
     login_request.code = code
     login_request = utils.database.Database().update_login_request(login_request)
     return login_request
+
 
 def post_user_data(session_value: str, state_value: str, url: str) -> bool:
     session = utils.session.get_user_session(session_value)
@@ -26,5 +27,4 @@ def post_user_data(session_value: str, state_value: str, url: str) -> bool:
         return False
     session["state"] = state_value
     resp = requests.post(url, json=session)
-    print(resp.status_code)
     return True
